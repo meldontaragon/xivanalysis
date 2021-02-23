5da20bc6202821f3e30be2bf72286cd4c8cb4e59
Melody Miller <mel@taragon.net>
2021-02-23 11:30:21 -0500

    Removed checks for faerie being out on start of fight, added CoreGauge functionality

--- src/parser/jobs/sch/modules/FaerieGauge.tsx
+++ src/parser/jobs/sch/modules/FaerieGauge.tsx
@@ -1,69 +1,74 @@
 import {t} from '@lingui/macro'
 import {Trans} from '@lingui/react'
-import Color from 'color'
 import {ActionLink} from 'components/ui/DbLink'
-import TimeLineChart from 'components/ui/TimeLineChart'
 import {getDataBy} from 'data'
 import ACTIONS from 'data/ACTIONS'
 import JOBS from 'data/JOBS'
 import STATUSES from 'data/STATUSES'
-import Module from 'parser/core/Module'
-import {Suggestion, TieredSuggestion, SEVERITY} from 'parser/core/modules/Suggestions'
-import React, {Fragment} from 'react'
+import {CastEvent, HealEvent} from 'fflogs'
+import {dependency} from 'parser/core/Module'
+import Combatants from 'parser/core/modules/Combatants'
+import Cooldowns from 'parser/core/modules/Cooldowns'
+import {CounterGauge, Gauge as CoreGauge} from 'parser/core/modules/Gauge'
+import Suggestions, {SEVERITY, TieredSuggestion} from 'parser/core/modules/Suggestions'
+import React from 'react'
+
+interface GaugeModifier {
+	[key: string]: number | undefined
+}
+
+// Actions that generate fairy gauge
+const FAERIE_MODIFIERS = new Map<number, GaugeModifier>([
+	// Generators
+	[ACTIONS.SCH_ENERGY_DRAIN.id, {cast: 10}],
+	[ACTIONS.LUSTRATE.id, {cast: 10}],
+	[ACTIONS.INDOMITABILITY.id, {cast: 10}],
+	[ACTIONS.SACRED_SOIL.id, {cast: 10}],
+	[ACTIONS.EXCOGITATION.id, {cast: 10}],
+
+	// Spenders
+	[ACTIONS.FEY_BLESSING.id, {cast: -10}],
+	[ACTIONS.FEY_UNION.id, {cast: -10}],
+	[STATUSES.FEY_UNION.id, {heal: -10}],
+	// potential issue: FEY_UNION actually spends more if it's kept up (10 gauge per tick)
+])
+
+// Severity markers for overcap
+// Wasting gauge is a relatively small loss as the two abilities it can be spent on are fairly low potency and typically should only be used if you have a free weave slot
+const GAUGE_WASTE_SEVERITY = {
+	50: SEVERITY.MINOR,
+}
 
 const SUMMON_ACTIONS = [
 	ACTIONS.SUMMON_EOS.id,
 	ACTIONS.SUMMON_SELENE.id,
 ]
-// Actions that generate fairy gauge
-const GAUGE_GENERATORS = [
-	ACTIONS.SCH_ENERGY_DRAIN.id,
-	ACTIONS.LUSTRATE.id,
-	ACTIONS.INDOMITABILITY.id,
-	ACTIONS.SACRED_SOIL.id,
-	ACTIONS.EXCOGITATION.id,
-]
-// Gauge limits
-const GAUGE_MAX = 100
-const GAUGE_START = 0
-const GAUGE_GAIN_AMOUNT = 10
-// Graph color
-const GRAPH_COLOR = Color(JOBS.SCHOLAR.colour)
-const BG_COLOR_FADE = 0.8
-const BORDER_COLOR_FADE = 0.5
-// Severity markers for overcap
-// Start at 30 because you can overcap when seraph is out and you can't drain the gauge
-const GAUGE_WASTE_SEVERITY = {
-	50: SEVERITY.MINOR,
-}
 
-export default class FaerieGauge extends Module {
-	static handle = 'fairieGauge'
+export class FaerieGauge extends CoreGauge {
 	static title = t('sch.gauge.title')`Faerie Gauge Usage`
-	static dependencies = [
-		'combatants',
-		'suggestions',
-	]
 
-	// Defaults
-	_gauge = GAUGE_START
-	_waste = 0
-	_history = []
-	_fairyOut = false
-	_noFairyAtStart = false
+	@dependency private combatants!: Combatants
+	@dependency private suggestions!: Suggestions
 
-	constructor(...args) {
-		super(...args)
+	private faerieGauge = this.add(new CounterGauge({
+		chart: {label: 'Faerie Gauge', color: JOBS.SCHOLAR.colour},
+	}))
+	private _fairyOut = false
 
-		// consumers
-		this.addEventHook('heal', {by: 'pet', abilityId: STATUSES.FEY_UNION.id}, this._onGaugeSpend)
-		this.addEventHook('cast', {by: 'player', abilityId: ACTIONS.SCH_FEY_BLESSING.id}, this._onGaugeSpend)
-		this.addEventHook('death', {to: 'player'}, this._onDeath) // I mean.. it does consume all your gauge..
-		// generators
-		this.addEventHook('cast', {by: 'player', abilityId: GAUGE_GENERATORS}, this._onGaugeGenerate)
-		this.addEventHook('complete', this._onComplete)
-		// summoning a fairy
-		this.addEventHook('cast', {by: 'player', abilityId: SUMMON_ACTIONS}, this._onSummon)
+	protected init() {
+		super.init()
+
+		this.addEventHook(
+			['cast', 'heal'],
+			{by: ['player', 'pet'], abilityId: Array.from(FAERIE_MODIFIERS.keys())},
+			this.onGaugeModifier,
+		)
+		this.addEventHook('complete', this.onComplete)
+		this.addEventHook(
+			'cast',
+			{by: 'player', abilityId: SUMMON_ACTIONS},
+			this.onSummon,
+		)
 	}
 
 	// Search through the events to figure out if there was a fairy out before logs started
@@ -73,12 +78,6 @@ export default class FaerieGauge extends Module {
 
 			const action = getDataBy(ACTIONS, 'id', event.ability.guid)
 			if (!action) { continue }
-
-			// if the first action we find is a summon action, bail out and give a warning later
-			if (action.id && SUMMON_ACTIONS.includes(action.id)) {
-				this._noFairyAtStart = true
-				break
-			}
 
 			const pet = this.parser.report.friendlyPets.find(pet => pet.id === event.sourceID)
 				|| {petOwner: -1}
@@ -99,88 +98,36 @@ export default class FaerieGauge extends Module {
 		return events
 	}
 
-	_onSummon() {
+	onSummon() {
 		this._fairyOut = true
 	}
 
-	// both spenders and generators consume the same amount
-	_onGaugeSpend() {
-		this._gauge -= GAUGE_GAIN_AMOUNT
-		this._updateHistory()
-	}
+	onGaugeModifier(event: CastEvent | HealEvent) {
+		const modifiers = FAERIE_MODIFIERS.get(event.ability.guid) || {}
 
-	_onGaugeGenerate() {
+		let amount = modifiers[event.type] || 0
+
+		// can only generate gauge if you have a Faerie out
 		if (this.combatants.selected.hasStatus(STATUSES.DISSIPATION.id) || ! this._fairyOut) {
-			// can't generate a guage without the fairy, so bail out
-			return
+			amount = 0
 		}
-
-		this._gauge += GAUGE_GAIN_AMOUNT
-		// figure out if we've capped and add to waste
-		if (this._gauge > GAUGE_MAX) {
-			this._gauge = GAUGE_MAX
-			this._waste += GAUGE_GAIN_AMOUNT
-		}
-
-		this._updateHistory()
+		this.faerieGauge.modify(amount)
 	}
 
 	_onDeath() {
 		this._fairyOut = false
-		this._gauge = 0
-		this._updateHistory()
 	}
 
-	_updateHistory() {
-		const ts = this.parser.currentTimestamp - this.parser.fight.start_time
-		this._history.push({t: ts, y: this._gauge})
-	}
-
-	_onComplete() {
-		// Warn on fairy not being out at the start
-		if (this._noFairyAtStart) {
-			this.suggestions.add(new Suggestion({
-				icon: ACTIONS.SUMMON_SERAPH.icon,
-				severity: SEVERITY.MEDIUM,
-				content: <Trans id="sch.gauge.nofairyatstart.content">
-					Your fairy is a considerable amount of your healing kit. Make sure to use <ActionLink {...ACTIONS.SUMMON_EOS}/> or <ActionLink {...ACTIONS.SUMMON_SELENE}/> prior to the start of combat
-				</Trans>,
-				why: <Trans id="sch.gauge.nofairyatstart.why">Your fairy was not summoned at the start of combat.</Trans>,
-			}))
-		}
+	private onComplete() {
 
 		// Suggest that they use their gauge consumers at certain overcap points
+		const {overCap} = this.faerieGauge
 		this.suggestions.add(new TieredSuggestion({
 			icon: ACTIONS.FEY_BLESSING.icon,
 			tiers: GAUGE_WASTE_SEVERITY,
-			value: this._waste,
+			value: overCap,
 			content: <Trans id="sch.gauge.waste.suggestion.content">Try to make use of your Faerie Gauge abilities <ActionLink {...ACTIONS.FEY_UNION}/> and <ActionLink {...ACTIONS.FEY_BLESSING}/>, since they are free oGCD heals that come naturally from using Aetherflow abilities.</Trans>,
-			why: <Trans id="sch.gauge.waste.suggestion.why">A total of {this._waste} gauge was lost due to exceeding the cap.</Trans>,
+			why: <Trans id="sch.gauge.waste.suggestion.why">A total of {overCap} gauge was lost due to exceeding the cap.</Trans>,
 		}))
-	}
-
-	// Generate the gauge graph
-	output() {
-		const data = {
-			datasets: [
-				{
-					label: 'Faerie Gauge',
-					steppedLine: true,
-					data: this._history,
-					backgroundColor: GRAPH_COLOR.fade(BG_COLOR_FADE).toString(),
-					borderColor: GRAPH_COLOR.fade(BORDER_COLOR_FADE).toString(),
-				},
-			],
-		}
-
-		return <Fragment>
-			<TimeLineChart
-				data={data}
-			/>
-			<Trans id="sch.gauge.waste">
-				A total of {this._waste} Faerie Gauge was wasted due to overcapping.
-			</Trans>
-		</Fragment>
-
 	}
 }
